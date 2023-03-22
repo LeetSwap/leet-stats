1cc1c3d7b4e0d5e53e37ee7734e628b0e5917772
Gilgames000 <22778436+Gilgames000@users.noreply.github.com>
2023-03-22 01:16:07 +0100

    chore: initial commit from create-ponder

---
+++ ponder.config.ts
@@ -0,0 +1,16 @@
+import type { PonderConfig } from "@ponder/core";
+
+export const config: PonderConfig = {
+  networks: [
+    { name: "mainnet", chainId: 1, rpcUrl: process.env.PONDER_RPC_URL_1 },
+  ],
+  contracts: [
+    {
+      name: "ExampleContract",
+      network: "mainnet",
+      address: "0x0",
+      abi: "./abis/ExampleContract.json",
+      startBlock: 1234567,
+    },
+  ],
+};
